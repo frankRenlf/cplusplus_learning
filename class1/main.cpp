b9233a81ebba16b69c15ad2503ba0b09aedc335c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-17 22:40:45 +0800

    format

--- class1/main.cpp
+++ class1/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 namespace R {
 	int rand = 10;
 }
-int test1(int a=3) {
+int test1(int a = 3) {
 	return a * a;
 }
-int test1(int a,int b) {
+int test1(int a, int b) {
 	return a * a;
 }
 int& Add(int a, int b)
@@ -23,6 +23,6 @@ int main()
 	int& ret = Add(1, 2);
 	Add(3, 4);
 	cout << "Add(1, 2) is :" << ret << endl;
-	cout <<  R::rand << endl;
+	cout << R::rand << endl;
 	return 0;
 }
