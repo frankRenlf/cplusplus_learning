f98211d0e229f95a0c31a49600878b1d97424c97
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-17 23:22:25 +0800

    update

--- class1/main.cpp
+++ class1/main.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 
 #include"head.h"
-#include <stdlib.h>
 
 using namespace std;
 namespace R {
 	int rand = 10;
+	int a = 1;
 }
 int test1(int a = 3) {
 	return a * a;
@@ -18,11 +18,12 @@ int& Add(int a, int b)
 	int c = a + b;
 	return c;
 }
+using R::a;
 int main()
 {
 	int& ret = Add(1, 2);
 	Add(3, 4);
 	cout << "Add(1, 2) is :" << ret << endl;
-	cout << R::rand << endl;
+	cout << a << endl;
 	return 0;
 }
