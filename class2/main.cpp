1ffeb45a2083462e07539d4a2d94e5eaf6b169dd
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-21 14:14:48 +0800

    update

--- class2/main.cpp
+++ class2/main.cpp
@@ -36,10 +36,13 @@ int& Add(int a, int b)
 	int c = a + b;
 	return c;
 }
-int main()
-{
-	int& ret = Add(1, 2);
-	Add(3, 4);
-	cout << "Add(1, 2) is :" << ret << endl;
-	return 0;
+//int main()
+//{
+//	int& ret = Add(1, 2);
+//	Add(3, 4);
+//	cout << "Add(1, 2) is :" << ret << endl;
+//	return 0;
+//}
+int main() {
+
 }
