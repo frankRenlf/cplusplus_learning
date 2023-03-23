ec9c49e9b8ea65f6eced39c67cb61de01640758b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-23 14:10:47 +0800

    update

--- class3/main.cpp
+++ class3/main.cpp
@@ -1,17 +1,28 @@
 #include<iostream>
 #include <vector>
 using namespace std;
+class B {
+public:
+	B() {
+		this->b = 2;
+	}
+private:
+	int b;
+};
 class A {
 public:
+	A() {
+		a = 1;
+	}
 	void show() {
 		cout << "show()" << this->a << endl;
 	}
 private:
 	int a;
+	B b;
 };
 int main() {
-	A* a = nullptr;
-	a->show();
+	A a;
 	return 0;
 
 }
