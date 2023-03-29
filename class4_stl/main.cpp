fb51e780d80b6ea88cd277170d260ea19b289087
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-29 14:37:20 +0800

    update

--- class4_stl/main.cpp
+++ class4_stl/main.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
 
 using namespace std;
+class Solution {
+public:
+	string reverseOnlyLetters(string s) {
+		int n = s.size();
+		string::iterator left = s.begin(), right = s.end() - 1;
+		while (true) {
+			while (left < right && !isalpha(*left)) { // ÅÐ¶Ï×ó±ßÊÇ·ñÉ¨Ãèµ½×ÖÄ¸
+				left++;
+			}
+			while (right > left && !isalpha(*right)) { // ÅÐ¶ÏÓÒ±ßÊÇ·ñÉ¨Ãèµ½×ÖÄ¸
+				right--;
+			}
+			if (left >= right) {
+				break;
+			}
+			swap(*left, *right);
+			left++;
+			right--;
+		}
+		return s;
+	}
+};
 
 int main() {
 	string* s = new string("123");
