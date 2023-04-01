e10885f1d280a5619ecb0089cbae31b1ffa22d82
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-01 15:11:58 +0800

    update

---
+++ class4_stl/leetcode.cpp
@@ -0,0 +1,8 @@
+#include"head.hpp"
+#include<cstring>
+
+int main() {
+	Solution s;
+	cout << s.reverseWords("Let's take LeetCode contest");
+	return 0;
+}
---
+++ class4_stl/string_test.cpp
@@ -0,0 +1,23 @@
+#include"head.hpp"
+
+//int main() {
+//
+//	string s1("123");
+//	char* s = new char[5] {'a', 'b', 'c', 'd', '\0'};
+//	cout << strlen(s) << endl;
+//	for (int i = 0; i < 4; i++)
+//	{
+//		s[i] += i;
+//	}
+//	cout << s << endl;
+//	string s2(s);
+//	cout << s2.size() << endl;
+//	size_t pos = s2.find("ce");
+//	if (pos != string::npos) {
+//		cout << s2.substr(pos);
+//	}
+//	else {
+//		cout << "fail" << endl;
+//	}
+//	return 0;
+//}
