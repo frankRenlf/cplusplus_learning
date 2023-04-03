f03d184ecea62bc1396d99f2778137046ae54bd3
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-03 11:18:46 +0800

    update

--- class4_stl/mystring.h
+++ class4_stl/mystring.h
@@ -165,13 +165,21 @@ namespace frank {
 			return _str[index];
 		}
 		// 返回c在string中第一次出现的位置
-		size_t find(char c, size_t pos = 0) const;
+		size_t find(char c, size_t pos = 0) const {
+
+		}
 		// 返回子串s在string中第一次出现的位置
-		size_t find(const char* s, size_t pos = 0) const;
+		size_t find(const char* s, size_t pos = 0) const {
+
+		}
 
 		// 在pos位置上插入字符c/字符串str，并返回该字符的位置
-		string& insert(size_t pos, char c);
-		string& insert(size_t pos, const char* str);
+		string& insert(size_t pos, char c) {
+
+		}
+		string& insert(size_t pos, const char* str) {
+
+		}
 
 		// 删除pos位置上的元素，并返回该元素的下一个位置
 		string& erase(size_t pos, size_t len);
