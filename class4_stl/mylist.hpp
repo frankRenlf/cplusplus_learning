fa2f324f8df50046b5946b9ae88cd98a2bf8daba
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-07 09:28:02 +0800

    update

---
+++ class4_stl/mylist.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+#include <cassert>
+#define DEFAULT_SIZE 4
+using std::cout;
+using std::endl;
+using std::ostream;
+using std::istream;
+
+namespace frank {
+	class list {
+	public:
+	private:
+	};
+}
